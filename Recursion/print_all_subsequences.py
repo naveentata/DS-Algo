aae1e5f18a072718c47cc9dc4d126b5841b0383f
Naveen Tata <naveenstuntert@gmail.com>
2022-04-17 12:20:02 +0530

    Update print_all_subsequences.py

--- Recursion/print_all_subsequences.py
+++ Recursion/print_all_subsequences.py
@@ -3,10 +3,11 @@ def fun(idx, temp_arr, arr, n):
     if idx == n:
         print(temp_arr)
         return
-    fun(idx+1, temp_arr, arr, n)
+    
     temp_arr.append(arr[idx])
     fun(idx+1, temp_arr, arr, n)
     temp_arr.pop()
+    fun(idx+1, temp_arr, arr, n)
     
 
 a = ['n', 'a', 'v', 'e', 'e', 'n']
