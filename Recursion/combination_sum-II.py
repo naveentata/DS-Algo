4c9b63add0ec7776593021663a9baeb896a0e5dd
Naveen Tata <naveenstuntert@gmail.com>
2022-04-17 22:13:40 +0530

    Create combination_sum-II.py

---
+++ Recursion/combination_sum-II.py
@@ -0,0 +1,26 @@
+# print("combination sumII")
+def fun(idx, temp_arr,  k, arr):
+
+    if k == 0:
+        print(temp_arr)
+        return
+    # Take idx
+    for i in range(idx, len(arr), 1):
+        if arr[i] > k:
+            break
+        if i > idx and arr[i] == arr[i-1]:
+            continue
+        
+        temp_arr.append(arr[i])
+        fun(i+1, temp_arr, k-arr[i], arr)
+        temp_arr.remove(temp_arr[len(temp_arr) - 1])
+
+a = [10,1,2,7,6,1,5]
+a.sort(reverse=False)
+fun(0, [], 8, a)
+
+
+# [1, 1, 6]
+# [1, 2, 5]
+# [1, 7]
+# [2, 6]
